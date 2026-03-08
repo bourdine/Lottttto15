c3281b50917d75979cebf0a2ba5704d062449f5e
Andrei Bourdine <bourdine@gmail.com>
2026-03-08 15:45:06 +0300

    Update WalletViewModelFactory.kt

--- app/src/main/java/com/example/lottttto11/viewmodel/WalletViewModelFactory.kt
+++ app/src/main/java/com/example/lottttto11/viewmodel/WalletViewModelFactory.kt
@@ -2,17 +2,19 @@ package com.example.lottttto11.viewmodel
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.lottttto11.data.UserDao
 import com.example.lottttto11.data.WalletDao
 
-class WalletViewModelFactory(
+class StatisticsViewModelFactory(
     private val userId: Int,
+    private val userDao: UserDao,
     private val walletDao: WalletDao
 ) : ViewModelProvider.Factory {
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        if (modelClass.isAssignableFrom(WalletViewModel::class.java)) {
+        if (modelClass.isAssignableFrom(StatisticsViewModel::class.java)) {
             @Suppress("UNCHECKED_CAST")
-            return WalletViewModel(userId, walletDao) as T
+            return StatisticsViewModel(userId, userDao, walletDao) as T
         }
-        throw IllegalArgumentException("Unknown ViewModel class")
+        throw IllegalArgumentException("Unknown ViewModel class: ${modelClass.name}")
     }
 }
